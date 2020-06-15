27419e3341b6aa425003769c77d3ad0d0f959801
tlac980 <thomas.lacombe@auckland.ac.nz>
2020-06-15 17:41:14 +1200

    Script + result example for the online tuning

--- examples_tuning/arf_trees_tuning/script_experiments_tuning_trees_arf.py
+++ examples_tuning/arf_trees_tuning/script_experiments_tuning_trees_arf.py
@@ -1,12 +1,13 @@
 import os
 
+from skmultiflow.data import FileStream
 from skmultiflow.meta import AdaptiveRandomForest
 from skmultiflow.drift_detection import ADWIN
 
 from skika.data.random_rbf_generator_redund import RandomRBFGeneratorRedund
 from skika.data.hyper_plane_generator_redund import HyperplaneGeneratorRedund
 from skika.data.stream_generator_redundancy_drift import StreamGeneratorRedund
-from skika.hyper_parameter_tuningevaluate_prequential_and_adapt import EvaluatePrequentialAndAdaptTreesARF
+from skika.hyper_parameter_tuning.evaluate_prequential_and_adapt import EvaluatePrequentialAndAdaptTreesARF
 
 
 from joblib import Parallel, delayed
@@ -46,7 +47,7 @@ def EvaluateModels(stream, run, n_trees, n_samples_max, n_samples_meas, metaMode
                                     metaKB=metaModel)
 
     # Run evaluation
-    evaluator.evaluate(stream=stream[0], model=modelsList, model_names=modelsNames)
+    model, acc = evaluator.evaluate(stream=stream[0], model=modelsList, model_names=modelsNames)
 
 ########################################################
 # Load the meta-model
@@ -58,7 +59,7 @@ dictMeta = {0.0:60 ,0.1:30, 0.2:30, 0.3:30, 0.4:60, 0.5:70, 0.6:60, 0.7:30, 0.8:
 n_run = 1
 nb_trees_init = 10
 stream= [StreamGeneratorRedund(base_stream = RandomRBFGeneratorRedund(n_classes=2, n_features=30, n_centroids=50, noise_percentage = 0.0), random_state=None, n_drifts = 10, n_instances = 10000),'RandomRBF']
-EvaluateModels(stream = stream, run = n_run, n_trees = nb_trees_init, n_samples_max = 10000, n_samples_meas = 500, metaModel=dictMeta)
+EvaluateModels(stream = stream, run = n_run, n_trees = nb_trees_init, n_samples_max = 5000, n_samples_meas = 500, metaModel=dictMeta)
 # ###########
 
 ## Example 2 : 1 adaptive run with Electricity dataset
@@ -66,7 +67,7 @@ EvaluateModels(stream = stream, run = n_run, n_trees = nb_trees_init, n_samples_
 #
 # nb_runs = 1
 # 
-# stream = [data.FileStream(./third_party/scikit_multiflow/src/skmultiflow/data/datasets/elec.csv'), 'Electricity']
+# stream = [FileStream('./third_party/scikit_multiflow/src/skmultiflow/data/datasets/elec.csv'), 'Electricity']
 #
 # nb_trees_init = 10
 # EvaluateModels(stream = stream, run = n_run, n_trees = nb_trees_init, n_samples_max = 45312, n_samples_meas = 200, metaModel=dictMeta)
@@ -77,7 +78,7 @@ EvaluateModels(stream = stream, run = n_run, n_trees = nb_trees_init, n_samples_
 #
 # nb_runs = 1
 # 
-# stream = [data.FileStream(./third_party/scikit_multiflow/src/skmultiflow/data/datasets/covtype.csv'), 'CoverType']
+# stream = [FileStream('./third_party/scikit_multiflow/src/skmultiflow/data/datasets/covtype.csv'), 'CoverType']
 #
 # nb_trees_init = 10
 # EvaluateModels(stream = stream, run = n_run, n_trees = nb_trees_init, n_samples_max = 45312, n_samples_meas = 200, metaModel=dictMeta)
@@ -105,7 +106,7 @@ EvaluateModels(stream = stream, run = n_run, n_trees = nb_trees_init, n_samples_
 # nb_runs = 30
 # nbTrees = [10,60,120]
 # n_cores = 4
-# datastream = [data.FileStream(./third_party/scikit_multiflow/src/skmultiflow/data/datasets/elec.csv'), 'Covtype']
+# datastream = [FileStream('./third_party/scikit_multiflow/src/skmultiflow/data/datasets/elec.csv'), 'Covtype']
 # for nbTreesInit in nbTrees :
 #     Parallel(n_jobs=n_cores, verbose=10)(delayed(EvaluateModels)(stream = datastream, run = n_run, n_trees = nbTreesInit, n_samples_max = 45312, n_samples_meas = 200, metaModel=None) for n_run in range(nb_runs))
 #
