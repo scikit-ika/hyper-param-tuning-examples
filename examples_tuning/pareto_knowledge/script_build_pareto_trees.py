e7cc90be866069a418e01ef8f6adec4cd5e7b2bb
tlac980 <thomas.lacombe@auckland.ac.nz>
2020-06-15 17:42:52 +1200

    Added script to build pareto for ARF trees

---
+++ examples_tuning/pareto_knowledge/script_build_pareto_trees.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 16:48:09 2020
+
+@author: tlac980
+"""
+
+import os
+
+from skika.hyper_parameter_tuning.build_pareto_knwledge_trees import buildTreesKnowledge
+
+"""
+ Script to generate pareto fronts from files generated with the knowledge script. 
+ 
+"""
+
+# Names of the evaluated models' names
+names = ['ARF10','ARF30','ARF60','ARF70','ARF90','ARF100','ARF120','ARF150','ARF200']    
+# Percentages of redundancy in the evaluated streams
+perc_redund = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+
+# Output directory for the output file
+output_dir = os.getcwd()
+# Path to the file containing the results from the knowledge script
+# Results must be processed and shaped like the example files (summary of kappa and RAM per hour performances)
+name_file =' /examples/pareto_knowledge/ExamplesTreesKnowledge/Results10-200.csv' # Available in hyper-param-tuning-examples repository
+
+# Processing of the pareto fronts
+paretoBuild = buildTreesKnowledge(results_file = name_file, list_perc_redund = perc_redund, list_models = names, output = output_dir, verbose = True)
+paretoBuild.load_drift_data()
+paretoBuild.calculatePareto()
+paretoBuild.bestConfig
